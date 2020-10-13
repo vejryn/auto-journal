2db6bc8ec5d9a37b3cd86b0a75c5cb1b51972098
vejryn <envyero@gmail.com>
2020-10-14 03:06:22 +0700

    Modifying main script.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import gspread
 import pprint
 from time import sleep
 from lib import bitmex
-from _setting import API_KEY, API_SECRET, API_BASE
+from setting import API_KEY, API_SECRET, API_BASE
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
@@ -28,39 +28,71 @@ query = {
     'count': count
 }
 
-position = connector._curl_bitmex(path="position", verb="GET", timeout=10)
-history = connector._curl_bitmex(path="execution/tradeHistory", verb="GET", query=query, timeout=10)
-wallet = connector._curl_bitmex(path="user/walletHistory", verb="GET", query=query, timeout=10)
+def retrieve():
+    position = connector._curl_bitmex(path="position", verb="GET", timeout=10)
+    history = connector._curl_bitmex(path="execution/tradeHistory", verb="GET", query=query, timeout=10)
+    wallet = connector._curl_bitmex(path="user/walletHistory", verb="GET", query=query, timeout=10)
+    data = worksheet.get_all_records()
 
-data = worksheet.get_all_records()
-status_cell = worksheet.find("open")
-for i in range(len(position)):
-    if position[i]['isOpen'] == True:
-        new_rows = len(data)+2
-        no = len(data)+1
-        symbol = position[i]['symbol']
-        open_date = datetime.strptime(position[i]['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y/%m/%d')
-        if position[i]['currentQty'] > 0:
-            _position = 'LONG'
-        else:
-            _position = 'SHORT'
+    try:
+        status_cell = worksheet.findall("open")
+    except gspread.CellNotFound:
+        status_cell = 0
+
+    open_symbol = []
+    for status in range(len(status_cell)):
+        open_symbol.append(worksheet.cell(status_cell[status].row, 2).value)
+
+    def get_rows(symbol):
+        for status in range(len(status_cell)):
+            if symbol == worksheet.cell(status_cell[status].row, 2).value:
+                rows = status_cell[status].row
+            else:
+                rows = 99
+        return rows
+
+    for i in range(len(position)):
         currentQty = position[i]['currentQty']
         entry = position[i]['avgEntryPrice']
-        worksheet.update_cell(new_rows, 1, no)
-        worksheet.update_cell(new_rows, 2, symbol)
-        worksheet.update_cell(new_rows, 3, open_date)
-        worksheet.update_cell(new_rows, 5, _position)
-        worksheet.update_cell(new_rows, 6, currentQty)
-        worksheet.update_cell(new_rows, 7, entry)
-        worksheet.update_cell(rows, 11, 'open')
-    if position[i]['isOpen'] == False and position[i]['symbol'] == worksheet.cell(status_cell.row, 2).value:
-        rows = status_cell.row
-        close_date = datetime.strptime(position[i]['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y/%m/%d')
-        _exit = history[0]['lastPx']
-        returns = position[i]['prevRealisedPnl'] / 100000000
-        final_balance = wallet[0]['walletBalance'] / 100000000
-        worksheet.update_cell(rows, 4, close_date)
-        worksheet.update_cell(rows, 8, _exit)
-        worksheet.update_cell(rows, 9, returns)
-        worksheet.update_cell(rows, 10, final_balance)
-        worksheet.update_cell(rows, 11, 'closed')
+        ## new trade
+        if position[i]['isOpen'] == True and position[i]['symbol'] not in open_symbol:
+            new_rows = len(data)+2
+            no = len(data)+1
+            symbol = position[i]['symbol']
+            open_date = datetime.strptime(position[i]['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y/%m/%d')
+            if position[i]['currentQty'] > 0:
+                _position = 'LONG'
+            else:
+                _position = 'SHORT'
+            worksheet.update_cell(new_rows, 1, no)
+            worksheet.update_cell(new_rows, 2, symbol)
+            worksheet.update_cell(new_rows, 3, open_date)
+            worksheet.update_cell(new_rows, 5, _position)
+            worksheet.update_cell(new_rows, 6, currentQty)
+            worksheet.update_cell(new_rows, 7, entry)
+            worksheet.update_cell(new_rows, 11, 'open')
+            print("New trade is recorded")
+        ## update trade
+        if position[i]['isOpen'] == True and position[i]['symbol'] in open_symbol:
+            rows = get_rows(position[i]['symbol'])
+            worksheet.update_cell(rows, 6, currentQty)
+            worksheet.update_cell(rows, 7, entry)
+            print("%s position is updated" % position[i]['symbol'])
+        ## close trade
+        if position[i]['isOpen'] == False and position[i]['symbol'] in open_symbol:
+            rows = get_rows(position[i]['symbol'])
+            close_date = datetime.strptime(position[i]['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y/%m/%d')
+            exit = history[0]['lastPx']
+            returns = position[i]['prevRealisedPnl'] / 100000000
+            final_balance = wallet[0]['walletBalance'] / 100000000
+            worksheet.update_cell(rows, 4, close_date)
+            worksheet.update_cell(rows, 8, exit)
+            worksheet.update_cell(rows, 9, returns)
+            worksheet.update_cell(rows, 10, final_balance)
+            worksheet.update_cell(rows, 11, 'closed')
+        else:
+            print("No change in %s position" % position[i]['symbol'])
+
+while True:
+    retrieve()
+    sleep(3)
